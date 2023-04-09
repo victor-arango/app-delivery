9cbb6ecaa6d138d4e13fd61950ade198928cb648
Victor Arango <victorarangoquiroz@gmail.com>
2023-04-09 16:36:55 -0500

    formulario profile

---
+++ delivery/lib/src/features/presentations/profile_detail_page/components/avatar_view.dart
@@ -0,0 +1,41 @@
+import 'package:delivery/src/utils/my_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../utils/styles/box_decoration_shadows.dart';
+
+@immutable
+class AvatarView extends StatelessWidget {
+  final String backgroundImage;
+  const AvatarView({super.key, required this.backgroundImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          decoration: createDecorationWithShadows(
+              borderRadius: BorderRadius.circular(65)),
+          width: 142,
+          height: 142,
+          child: CircleAvatar(backgroundImage: NetworkImage(backgroundImage)),
+        ),
+        Transform.translate(
+          offset: const Offset(0, -35),
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+                color: MyColors.primaryColor,
+                borderRadius: BorderRadius.circular(20)),
+            child: Icon(
+              Icons.camera_alt,
+              color: MyColors.white,
+              size: 20,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
